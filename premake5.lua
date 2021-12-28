465b2e1e1bd5ca04f9421a5dd59a49ed4f92b582
Clément Darne <44375729+ClementDrn@users.noreply.github.com>
2021-12-28 10:31:32 +0100

    Added premake file

---
+++ premake5.lua
@@ -0,0 +1,25 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+  objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+    "include/**.h",
+    "src/**.h",
+    "src/**.c",
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
